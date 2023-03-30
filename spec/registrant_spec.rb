f66d8828627a9c1fd1e36b36f94856fa8010bbe6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 17:59:44 -0600

    Test: #earn_permit

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Registrant do
   before(:each) do
-    @registrant_1 = Registrant.new('Bruce', 18, true )
-    @registrant_2 = Registrant.new('Penny', 15 )
+    @registrant_1 = Registrant.new('Bruce', 18, true)
+    @registrant_2 = Registrant.new('Penny', 15)
   end
 
   describe '#initialize' do 
@@ -39,5 +39,10 @@ RSpec.describe Registrant do
     end
   end
 
-  
+  describe '#earn_permit' do 
+    it 'earns permit' do 
+      @registrant_2.earn_permit
+      expect(@registrant_2.permit?).to be true
+    end
+  end
 end
