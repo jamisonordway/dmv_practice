52c50ffbaa142a296d079c3fdfc7edbbc39613ab
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 17:35:02 -0600

    Fix: dmv.rb:12 change enumerable to find_all

--- lib/dmv.rb
+++ lib/dmv.rb
@@ -1,6 +1,6 @@
 class Dmv
   attr_reader :facilities
-  
+
   def initialize
     @facilities = []
   end
@@ -10,7 +10,7 @@ class Dmv
   end
 
   def facilities_offering_service(service)
-    @facilities.find do |facility|
+    @facilities.find_all do |facility|
       facility.services.include?(service)
     end
   end
