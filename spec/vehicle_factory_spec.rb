2dcdef7527e3d3038c344412dc763c8d00b2b5c0
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-31 15:54:56 -0600

    Test: #engine_type

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -18,18 +18,36 @@ RSpec.describe VehicleFactory do
   end
 
   describe '#create_vehicles' do 
-    it 'create vehicles from data' do 
-      @factory.create_vehicles(@wa_ev_registrations)
+    xit 'create vehicles from data' do 
+      @factory.create_wa_vehicles(@wa_ev_registrations)
       expect(@factory.vehicles).to_not be_empty
       expect(@factory.vehicles[0]).to be_a Vehicle
     end
   end
 
+  describe '#engine_type' do 
+    it 'can decipher engines' do 
+      expect(@factory.engine_type('PROPANE')).to eq(:combustion)
+      expect(@factory.engine_type('ELECTRIC')).to eq(:ev)
+      expect(@factory.engine_type('AN ACTUAL HORSE')).to eq(:wtf)
+    end
+  end
+
   describe '#create_vehicles from new york' do 
-    it 'create vehicles from data' do 
-      @factory.create_vehicles(@ny_registrations)
+    xit 'create vehicles from data' do 
+      @factory.create_ny_vehicles(@ny_registrations)
       expect(@factory.vehicles).to_not be_empty
       expect(@factory.vehicles[0]).to be_a Vehicle
+    end
+
+    xit 'vehicle methods can be called on vehicles created' do 
+      @factory.create_ny_vehicles(@ny_registrations)
+      require 'pry'; binding.pry
+      expect(@factory.vehicles[0].vin).to eq('9999236')
+      expect(@factory.vehicles[0].year).to eq(1937)
+      expect(@factory.vehicles[0].model).to eq('4DSD')
+      expect(@factory.vehicles[0].engine).to eq(:gas)
+      expect(@factory.vehicles[0].antique?).to be true
     end
   end
 end
