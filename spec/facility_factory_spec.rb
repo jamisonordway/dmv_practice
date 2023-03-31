4bf2192c2184b4c36683fdce5cda6fc792519956
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-31 14:17:01 -0600

    Test: #create_ny_facilities && #create_mo_facilities & refactor #initialize test to include arrays for each stat facility type

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -13,8 +13,10 @@ RSpec.describe FacilityFactory do
       expect(@factory).to be_a FacilityFactory
     end
 
-    it 'begins without oregon facilities' do 
-      expect(@factory.or_facilities).to eq( [])
+    it 'begins without facilities of any type' do 
+      expect(@factory.or_facilities).to eq([])
+      expect(@factory.ny_facilities).to eq([])
+      expect(@factory.mo_facilities).to eq([])
     end
   end
 
@@ -30,6 +32,50 @@ RSpec.describe FacilityFactory do
       expect(@factory.or_facilities[0].name).to eq('Albany DMV Office')
       expect(@factory.or_facilities[0].address).to eq('2242 Santiam Hwy SE Albany OR 97321')
       expect(@factory.or_facilities[0].phone).to eq('541-967-2014')
+    end
+  end
+  describe '#create_or_facilities' do 
+    it 'create facilities from data' do 
+      @factory.create_or_facilities(@oregon_facilities)
+      expect(@factory.or_facilities).to_not be_empty
+      expect(@factory.or_facilities[0]).to be_a Facility
+    end
+
+    it 'facilities instantiate with attributes' do 
+      @factory.create_or_facilities(@oregon_facilities)
+      expect(@factory.or_facilities[0].name).to eq('Albany DMV Office')
+      expect(@factory.or_facilities[0].address).to eq('2242 Santiam Hwy SE Albany OR 97321')
+      expect(@factory.or_facilities[0].phone).to eq('541-967-2014')
+    end
+  end
+
+  describe '#create_ny_facilities' do 
+    it 'create facilities from data' do 
+      @factory.create_ny_facilities(@new_york_facilities)
+      expect(@factory.ny_facilities).to_not be_empty
+      expect(@factory.ny_facilities[0]).to be_a Facility
+    end
+
+    it 'facilities instantiate with attributes' do 
+      @factory.create_ny_facilities(@new_york_facilities)
+      expect(@factory.ny_facilities[1].name).to eq('JAMESTOWN')
+      expect(@factory.ny_facilities[1].address).to eq('512 WEST 3RD STREET JAMESTOWN NY 14701')
+      expect(@factory.ny_facilities[1].phone).to eq('7166618220')
+    end
+  end
+  
+  describe '#create_mo_facilities' do 
+    xit 'create facilities from data' do 
+      @factory.create_mo_facilities(@missouri_facilities)
+      expect(@factory.mo_facilities).to_not be_empty
+      expect(@factory.mo_facilities[0]).to be_a Facility
+    end
+
+    xit 'facilities instantiate with attributes' do 
+      @factory.create_mo_facilities(@missouri_facilities)
+      expect(@factory.mo_facilities[0].name).to eq('OAKVILLE')
+      expect(@factory.mo_facilities[0].address).to eq('3164 TELEGRAPH ROAD ST LOUIS MO 63125')
+      expect(@factory.mo_facilities[0].phone).to eq('(314) 887-1050')
     end
   end
 end
