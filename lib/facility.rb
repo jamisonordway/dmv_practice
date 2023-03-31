e1540cb77e5f9377deb50e0c2b4fda00182d5d04
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 18:46:15 -0600

    Feat: #registered vehicles & #collected_fees

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,13 +2,17 @@ class Facility
   attr_reader :name, 
               :address, 
               :phone, 
-              :services
+              :services, 
+              :registered_vehicles, 
+              :collected_fees
 
   def initialize(facility_details)
     @name = facility_details[:name]
     @address = facility_details[:address]
     @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
