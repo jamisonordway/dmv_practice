66524db7b40b1e902416427de4d1a32c0fda3154
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 19:44:58 -0600

    Fix: niggly details

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Facility do
       engine: :ice
     })
     @registrant_1 = Registrant.new('Bruce', 18, true)
-    @registrant_2 = Registrant.new('Penny', 15)
+    @registrant_2 = Registrant.new('Penny', 16)
     @registrant_3 = Registrant.new('Tucker', 15)
   end
 
@@ -95,7 +95,7 @@ RSpec.describe Facility do
     it 'written test only if facility_1 able' do 
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       expect(@registrant_1.permit?).to be true
-      expect(@facility_1.administer_written_test).to be false
+      expect(@facility_1.administer_written_test(@registrant_1)).to be false
       expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
       @facility_1.add_service('Written Test')
       expect(@facility_1.administer_written_test(@registrant_1)).to be true
@@ -117,7 +117,7 @@ RSpec.describe Facility do
       expect(@registrant_3.age).to eq(15)
       expect(@registrant_3.permit?).to be false
       expect(@facility_1.administer_written_test(@registrant_3)).to be false
-      expect(@registrant_3.earn_permit).to be false
+      @registrant_3.earn_permit
       expect(@facility_1.administer_written_test(@registrant_3)).to be false
       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
