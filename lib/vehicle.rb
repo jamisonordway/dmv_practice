0e92fc53e5b3081d1c8a965cf3efa8b64299f53c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-30 18:58:27 -0600

    Add plate_type and registration_date attributes

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,9 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine, 
-              :registration_date
+              :engine 
+  attr_accessor   :registration_date, 
+                  :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -15,6 +16,7 @@ class Vehicle
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
     @registration_date = nil
+    @plate_type = nil
   end
 
   def antique?
