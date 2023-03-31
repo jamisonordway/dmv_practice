7bc9685e5bdfabf7f90b8debeb28403a77361f5b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-31 14:06:02 -0600

    Feat: #create_or_facilities

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,7 +1,18 @@
 class FacilityFactory
-  attr_reader :facilities
+  attr_reader :or_facilities
   
   def initialize
-    @facilities = []
+    @or_facilities = []
+  end
+
+  def create_or_facilities(dmv_data)
+    dmv_data.map do |facility|
+      address_info = eval(facility[:location_1][:human_address])
+      @or_facilities << Facility.new({
+        name: facility[:title], 
+        address: address_info.values.join(' '), 
+        phone: facility[:phone_number] 
+      })
+    end
   end
 end
