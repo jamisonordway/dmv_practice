9e2aabcfbbe9134b840f98a37066090d760ac56d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-31 13:31:44 -0600

    Feat: #create_vehicles

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -4,4 +4,14 @@ class VehicleFactory
   def initialize
     @vehicles = []
   end
+
+  def create_vehicles(dmv_data)
+    dmv_data.map do |registration|
+      vehicles << Vehicle.new({
+        vin: registration[:vin_1_10], 
+        year: registration[:model_year].to_i, 
+        model: registration[:model], engine: :ev
+      })
+    end.flatten!
+  end
 end
