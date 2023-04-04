428cec09e80e6f32d52e42608e2ec4290398ca3b
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-04 17:26:28 -0600

    Fix: facilities instantiate with attributes test for missouri

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -86,15 +86,11 @@ RSpec.describe FacilityFactory do
       expect(@factory.mo_facilities[0].name).to eq('OAKVILLE')
       expect(@factory.mo_facilities[0].address).to eq('3164 TELEGRAPH ROAD ST LOUIS MO 63125')
       expect(@factory.mo_facilities[0].phone).to eq('(314) 887-1050')
-      expect(@factory.mo_facilities[0].daily_hours).to eq('Monday - Friday - 8:00 to 4:00')
-      expect(@factory.mo_facilities[0].holidays).to eq(
-        "Independence Day (07/04/22), Labor Day (09/05/22), 
-        Columbus Day (10/10/22), Veterans Day (11/11/22), Thanksgiving (11/24/22), 
-        Christmas Day Observed (12/26/22), New Year's Day Observed (01/02/23), 
-        Martin Luther King Day (01/16/23), Lincoln’s Birthday (02/13/23), President’s Day (02/20/23),
-        Truman’s Birthday (05/08/23), Memorial Day (05/29/23), Juneteenth (06/19/2023), 
-        Independence Day (07/04/23), Labor Day (09/04/23), Columbus Day (10/09/23), Veterans Day (11/10/23), 
-        Thanksgiving (11/23/23), Christmas Day Observed (12/25/23)")
+      expect(@factory.mo_facilities[0].daily_hours).to eq('Monday-Friday - 9:00 to 5:00, Last Saturday  - 9:00 to 12:00')
+      expect(@factory.mo_facilities[0].holidays).to eq("Thanksgiving (11/24/22), Christmas Day Observed (12/26/22), 
+      New Year's Day Observed (01/02/23), Martin Luther King Day (01/16/23), President’s Day (02/20/23), 
+      Memorial Day (05/29/23), Juneteenth (06/19/2023), Independence Day (07/04/23), Labor Day (09/04/23), 
+      Veterans Day (11/10/23), Thanksgiving (11/23/23), Christmas Day Observed (12/25/23)")
     end
   end
 end
