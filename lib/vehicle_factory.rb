8bd1be47857005480b76054fa2e8d40427bc9673
Kat Brandt <joyfulkat11@gmail.com>
2023-06-06 10:45:44 -0600

    Pare down code.

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -17,8 +17,6 @@ class VehicleFactory
     end.flatten!
   end
   
-  #helpers 
-  
   def engine_type(engine_type)
     if engine_type == 'GAS' || engine_type == 'DIESEL' || engine_type == 'PROPANE' || engine_type == 'FLEX'
       :combustion
@@ -27,83 +25,5 @@ class VehicleFactory
     else
       :wtf
     end
-  end
-  
-  #iteration 4
-  
-  def create_ny_vehicles(dmv_data)
-    dmv_data.map do |registration|
-      if registration[:record_type] == 'VEH'  
-        @vehicles << Vehicle.new({
-          vin: registration[:vin], 
-          year: registration[:model_year].to_i, 
-          make: registration[:make],
-          model: registration[:body_type], 
-          engine: engine_type(registration[:fuel_type])
-        })
-      end
-      @vehicles.flatten!
-    end
-  end
-  
-  def most_popular_model
-    model_vehicles = @vehicles.sort_by(&:model)
-    count_by_model = Hash.new(0)
-    model_vehicles.map do |vehicle|
-      count_by_model[vehicle.model] += 1 
-    end
-    most_pop_model = count_by_model.max_by{|model, amount| amount}.first
-    most_pop_model
-  end
-
-  def most_popular_make
-    make_vehicles = @vehicles.sort_by{|vehicle| vehicle.make}
-    count_by_make = Hash.new(0)
-    make_vehicles.map do |vehicle|
-      count_by_make[vehicle.make] += 1 
-    end
-    most_pop_make = count_by_make.max_by{|make, amount| amount}.first
-    "#{most_pop_make}"
-  end
-
-  def most_popular_model
-    model_vehicles = @vehicles.sort_by(&:model)
-    count_by_model = Hash.new(0)
-    model_vehicles.map do |vehicle|
-      count_by_model[vehicle.model] += 1 
-    end
-    most_pop_model = count_by_model.max_by{|model, amount| amount}.first
-    most_pop_model
-  end
-
-  def most_popular_make
-    make_vehicles = @vehicles.sort_by{|vehicle| vehicle.make}
-    count_by_make = Hash.new(0)
-    make_vehicles.map do |vehicle|
-      count_by_make[vehicle.make] += 1 
-    end
-    most_pop_make = count_by_make.max_by{|make, amount| amount}.first
-    "#{most_pop_make}"
-  end
-  
-  def most_popular_make
-    make_vehicles = @vehicles.sort_by{|vehicle| vehicle.make}
-    count_by_make = Hash.new(0)
-    make_vehicles.map do |vehicle|
-      count_by_make[vehicle.make] += 1 
-    end
-    most_pop_make = count_by_make.max_by{|make, amount| amount}.first
-    "#{most_pop_make}"
-  end
-
-  def count_by_model_year(yr)
-    count = @vehicles.count{|vehicle| vehicle.year == yr}
-    count
-  end
-
-  def most_registered_county(dmv_data)
-    counter = Hash.new(0)
-    dmv_data.map {|registration| counter[registration[:county]] += 1  }
-    counter.max_by{|county, num| num}.first
   end
 end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -21,13 +21,14 @@ RSpec.describe VehicleFactory do
   describe '#create_vehicles' do 
     it 'create vehicles from data' do 
       @factory.create_wa_vehicles(@wa_ev_registrations)
+      
       expect(@factory.vehicles).to_not be_empty
       expect(@factory.vehicles[0]).to be_a Vehicle
     end
     
     it 'washington vehicles have attributes' do 
       @factory.create_wa_vehicles(@wa_ev_registrations)
-      require 'pry'; binding.pry
+
       expect(@factory.vehicles[0].vin).to eq('WMEEJ9AA7E')
       expect(@factory.vehicles[0].year).to eq(2014)
       expect(@factory.vehicles[0].make).to eq('SMART')
@@ -36,6 +37,7 @@ RSpec.describe VehicleFactory do
 
     it 'New York vehicles have attributes' do 
       @factory.create_ny_vehicles(@ny_registrations)
+      
       expect(@factory.vehicles[0].vin).to eq('9999236')
       expect(@factory.vehicles[0].year).to eq(1937)
       expect(@factory.vehicles[0].make).to eq('CHRY')
@@ -49,55 +51,6 @@ RSpec.describe VehicleFactory do
       expect(@factory.engine_type('PROPANE')).to eq(:combustion)
       expect(@factory.engine_type('ELECTRIC')).to eq(:ev)
       expect(@factory.engine_type('AN ACTUAL HORSE')).to eq(:wtf)
-    end
-  end
-
-  describe '#create_vehicles from new york' do 
-    it 'create vehicles from data' do 
-      @factory.create_ny_vehicles(@ny_registrations)
-      expect(@factory.vehicles).to_not be_empty
-      expect(@factory.vehicles[0]).to be_a Vehicle
-    end
-
-    it 'vehicle methods can be called on vehicles created' do 
-      @factory.create_ny_vehicles(@ny_registrations)
-      expect(@factory.vehicles[0].vin).to eq('9999236')
-      expect(@factory.vehicles[0].year).to eq(1937)
-      expect(@factory.vehicles[0].model).to eq('4DSD')
-      expect(@factory.vehicles[0].engine).to eq(:combustion)
-      expect(@factory.vehicles[0].antique?).to be true
-    end
-  end
-
-  describe 'EV registration data' do 
-    xit '#most_popular_model' do 
-      @factory.create_wa_vehicles(@wa_ev_registrations)
-      expect(@factory.most_popular_model).to eq('Leaf')
-    end
-
-    xit '#most_popular_make' do 
-      @factory.create_wa_vehicles(@wa_ev_registrations)
-      expect(@factory.most_popular_make).to eq('TESLA')
-    end
-    
-    xit '#count_by_model_year for washington' do 
-      @factory.create_wa_vehicles(@wa_ev_registrations)
-      expect(@factory.count_by_model_year(2012)).to eq(31)
-    end
-
-    xit '#count_by_model_year for new york' do 
-      @factory.create_ny_vehicles(@ny_registrations)
-      expect(@factory.count_by_model_year(1968)).to eq(4)
-    end
-
-    xit '#most_registered_county for washington' do 
-      @factory.create_wa_vehicles(@wa_ev_registrations)
-      expect(@factory.most_registered_county(@wa_ev_registrations)).to eq('King')
-    end
-
-    xit '#most_registered_county for new york' do 
-      @factory.create_ny_vehicles(@ny_registrations)
-      expect(@factory.most_registered_county(@ny_registrations)).to eq('OUT-OF-STATE')
     end
   end
 end
